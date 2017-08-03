a19008866929540c52582f0668bfb6743c974377
Owen Pragel <opragel@users.noreply.github.com>
2017-08-02 19:54:51 -0700

    Made modifications

--- install_latest_slack_osx_app.sh
+++ install_latest_slack_osx_app.sh
@@ -1,46 +1,59 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# your funeral
+# seriously use autopkg or something
 
-# Downloads and installs the latest version of Slack for OS X
+DOWNLOAD_URL="https://slack.com/ssb/download-osx"
 
-printf "Slack auto-install script for OS X\n\n"
+APP_NAME="Slack.app"
+APP_PATH="/Applications/$APP_NAME"
+APP_VERSION_KEY="CFBundleShortVersionString"
 
-SLACK_DOWNLOAD_URL="https://slack.com/ssb/download-osx"
-SLACK_10_6_DOWNLOAD_URL="https://slack.com/ssb/download-osx-10-6"
-
-SLACK_ZIP_PATH="/tmp/Slack.app.zip"
 SLACK_UNZIP_DIRECTORY="/tmp"
 SLACK_APP_UNZIPPED_PATH="/tmp/Slack.app/"
-SLACK_APP_PATH="/Applications/Slack.app/"
-SLACK_RELATIVE_INFO_PLIST="Contents/Info.plist"
+
+currentSlackVersion=$(curl -s 'https://downloads.slack-edge.com/mac_releases/releases.json' | grep -o "[0-9]\.[0-9]\.[0-9]" | tail -1)
+
+if [ -d "$APP_PATH" ]; then
+    localSlackVersion=$(defaults read "$APP_PATH/Contents/Info.plist" "$APP_VERSION_KEY")
+    if [ "$currentSlackVersion" = "$localSlackVersion" ]; then
+        printf "Slack is already up-to-date. Version: %s" "$localSlackVersion"
+        exit 0
+    fi
+fi
 
 # OS X major release version
 osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
 
-if [ "$osvers" -lt 6 ]; then
-  printf "Slack is not available for Mac OS X 10.5.8 or earlier\n"
+if [ "$osvers" -lt 7 ]; then
+    printf "Slack is not available for Mac OS X 10.6 or earlier\n"
+    exit 403
+elif [ "$osvers" -ge 7 ]; then
+    finalDownloadUrl=$(curl "$DOWNLOAD_URL" -s -L -I -o /dev/null -w '%{url_effective}')
 else
-  rm -rf "$SLACK_ZIP_PATH" "$SLACK_APP_UNZIPPED_PATH"
-  if [ "$osvers" -lt 7 ]; then
-    printf "Downloading Slack for Mac OS X 10.6.8\n"
-    /usr/bin/curl --retry 3 -L "$SLACK_10_6_DOWNLOAD_URL" -o "$SLACK_ZIP_PATH"
-  else
-    printf "Downloading Slack for Mac OS X 10.7.5 and later\n"
-    /usr/bin/curl --retry 3 -L "$SLACK_DOWNLOAD_URL" -o "$SLACK_ZIP_PATH"
-  fi
-  /usr/bin/unzip -q "$SLACK_ZIP_PATH" -d "$SLACK_UNZIP_DIRECTORY"
-  if pgrep 'Slack'; then
-    printf "Error: Slack is currently running!\n"
-  else
-    if [ -d "$SLACK_APP_PATH" ]; then
-      slackVersion=$(defaults read "$SLACK_APP_PATH$SLACK_RELATIVE_INFO_PLIST" CFBundleShortVersionString)
-      printf "Removing Slack version %s\n" "$slackVersion"
-      rm -rf "$SLACK_APP_PATH"
-    fi
-    mv -f "$SLACK_APP_UNZIPPED_PATH" "$SLACK_APP_PATH"
-    chown -R root:wheel "$SLACK_APP_PATH"
-    slackVersion=$(defaults read "$SLACK_APP_PATH$SLACK_RELATIVE_INFO_PLIST" CFBundleShortVersionString)
-    printf "Installed Slack version %s\n" "$slackVersion"
-    rm -f "$SLACK_ZIP_PATH" "$SLACK_APP_UNZIPPED_PATH"
-  fi
+    printf "Unable to read OS version"
+    exit 404
 fi
-printf "Exiting...\n"
+
+zipName=$(printf "%s" "${finalDownloadUrl[@]}" | sed 's@.*/@@')
+slackZipPath="/tmp/$zipName"
+rm -rf "$slackZipPath" "$SLACK_UNZIP_DIRECTORY"
+/usr/bin/curl --retry 3 -L "$finalDownloadUrl" -o "$slackZipPath"
+/usr/bin/unzip -q "$slackZipPath" -d "$SLACK_UNZIP_DIRECTORY"
+rm -f "$slackZipPath"
+
+if pgrep 'Slack'; then
+    printf "Error: Slack is currently running!\n"
+    exit 409
+else
+    if [ -d "$APP_PATH" ]; then
+        rm -rf "$APP_PATH"
+    fi
+    mv -f "$SLACK_APP_UNZIPPED_PATH" "$APP_PATH"
+    # Slack permissions are stupid
+    chown -R root:admin "$APP_PATH"
+    localSlackVersion=$(defaults read "$APP_PATH/Contents/Info.plist" "$APP_VERSION_KEY")
+    if [ "$currentSlackVersion" = "$localSlackVersion" ]; then
+        printf "Slack is now updated/installed. Version: %s" "$localSlackVersion"
+        exit 0
+    fi
+fi
